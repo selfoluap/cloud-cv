ae5b323b0db77a237743b8b465c5442791c296c6
Paulo Liedtke <liedtke.paulo@gmail.com>
2023-11-25 23:51:01 +0900

    Added basic threlte scene

---
+++ fronted/src/lib/components/Scene.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { T, useFrame } from '@threlte/core'
+  import { interactivity, OrbitControls } from '@threlte/extras'
+  import { spring } from 'svelte/motion'
+  import World from './World.svelte';
+  import { DEG2RAD } from 'three/src/math/MathUtils'
+
+  let autoRotate = false
+  
+  interactivity()
+  const scale = spring(1)
+  let rotation = 0
+  useFrame((state, delta) => {
+    rotation += delta
+  })
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[0, 5, 10]}
+>
+  <OrbitControls
+  autoRotate={autoRotate}
+  enableZoom={false}
+  maxPolarAngle={DEG2RAD * 80}
+  />
+</T.PerspectiveCamera>
+
+<World
+></World>
+
+<T.DirectionalLight position={[0, 0, 0]} castShadow />
+
---
+++ fronted/src/lib/components/World.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { Text } from '@threlte/extras'
+    import { T } from '@threlte/core';
+    import {Pane} from 'tweakpane'; 
+    let value = "Paulo Liedtke"
+    let fontSize = 1
+    let fontColor = "white"
+/*     const pane = new Pane()
+    pane.addBlade({
+		view: 'text',
+		label: "Use the 'wasd' keys to move around",
+	}) */
+
+    function setFontSize() {
+        fontSize = fontSize == 1 ? 1.5 : 1
+    }
+</script>
+
+<Text text={value}
+color={fontColor}
+on:click={() => setFontSize()}
+{fontSize}
+anchorX="50%"
+anchorY="100%"
+></Text>
+
+
+
+<T.GridHelper args={[50, 50]} />
